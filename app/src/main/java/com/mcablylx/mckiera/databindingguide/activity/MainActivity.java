83d02c9cde9d24060914f1b1839bea653d4388f2
mcablylx <308380504@qq.com>
2016-04-19 14:45:16 +0800

    init

---
+++ app/src/main/java/com/mcablylx/mckiera/databindingguide/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mcablylx.mckiera.databindingguide.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mckiera on 2016-04-19.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
