ed42b700e9b9339e2775e06f9bd5431ec660dbfd
mcablylx <308380504@qq.com>
2016-04-19 14:58:44 +0800

    添加main_activity.xml

--- app/src/main/java/com/mcablylx/mckiera/databindingguide/activity/MainActivity.java
+++ app/src/main/java/com/mcablylx/mckiera/databindingguide/activity/MainActivity.java
@@ -1,7 +1,13 @@
 package com.mcablylx.mckiera.databindingguide.activity;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+
+import com.mcablylx.mckiera.databindingguide.R;
+import com.mcablylx.mckiera.databindingguide.bean.User;
+import com.mcablylx.mckiera.databindingguide.databinding.MainActivityBinding;
 
 /**
  * Created by Mckiera on 2016-04-19.
@@ -10,6 +16,9 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        MainActivityBinding mab =  DataBindingUtil.setContentView(this, R.layout.main_activity);
+        User user = new User("Name","Phone");
+        mab.setUser(user);
+        mab.setIsShow(false);
     }
 }
